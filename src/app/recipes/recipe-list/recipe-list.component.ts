8a477a3d22b3b763a104cd46627431b501c47a4f
balaji1610 <balajiece1610@gmail.com>
2022-03-31 16:18:19 +0530

    Work Nav

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+   recipes:Recipe[] = [
+    new Recipe("A Test Recipe","This is simply a test","https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg"),
+    new Recipe("A Test Recipe","This is simply a test","https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg")
+   ];
   constructor() { }
 
   ngOnInit(): void {
