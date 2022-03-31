812de1bec6cec04b551f0d8ec05fab46d71e3945
balaji1610 <balajiece1610@gmail.com>
2022-03-31 17:53:27 +0530

    Section 3 Stage

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients:Ingredient[] = [
+    new Ingredient('Apple',5),
+    new Ingredient('Orange',10)
+  ]
   constructor() { }
 
   ngOnInit(): void {
